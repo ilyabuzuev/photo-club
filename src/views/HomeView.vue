792b61ea98bc3c95c769d6acc731fd61dcedc4bb
ilyabuzuev <ilya.buzuev@mail.ru>
2024-09-22 10:48:56 +0300

    added project files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Client, Account, Databases } from 'appwrite';
+
+const client = new Client();
+
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('66efbca500050ed8a3da');
+
+const account = new Account(client);
+const database: Databases = new Databases(client);
+
+console.log(database);
+
+</script>
+
+<template>
+  <main>
+    <h1>hello, world</h1>
+  </main>
+</template>
